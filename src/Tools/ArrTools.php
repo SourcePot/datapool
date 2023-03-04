345f0f03c2c7a2cb46d29fee90540f70c692897d
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2023-03-04 13:20:26 +0100

    Comments added

--- src/Tools/ArrTools.php
+++ src/Tools/ArrTools.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* This file is part of the Datapool CMS package.
+* @package Datapool
+* @author Carsten Wallenhauer
+* @author Carsten Wallenhauer <admin@datapool.info>
+* @copyright 2023 to today Carsten Wallenhauer
+* @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-v3
+*/
 declare(strict_types=1);
 
 namespace Datapool\Tools;
