c76c72435e87dc9a84159011539a1ecb5fddb495
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2024-11-30 17:27:22 +0100

    Update Haystack.php

--- src/php/Foundation/Haystack.php
+++ src/php/Foundation/Haystack.php
@@ -46,35 +46,55 @@ class Haystack{
     
     public function getQueryHtml(array $arr):array
     {
+        $queryEntry=array('Source'=>$this->entryTable,'Group'=>'Queries','Folder'=>$this->oc['SourcePot\Datapool\Root']->getCurrentUserEntryId());
         // process data
         $formData=$this->oc['SourcePot\Datapool\Foundation\Element']->formProcessing($arr['callingClass'],$arr['callingFunction']);
         if (isset($formData['cmd']['search'])){
-            $serachResult=$this->getSerachResukltHtml($formData['val']['query']);
+            if (empty($formData['val']['Query'])){
+            } else {
+                $serachResult=$this->getSerachResultHtml($formData['val']['Query']);
+                $queryEntry['Name']=substr($formData['val']['Query'],0,20);
+                $queryEntry['Content']=array('Query'=>$serachResult['Query'],'Names'=>$serachResult['Names']);
+                $queryEntry['Params']=array(__CLASS__=>array('Hits'=>$serachResult['Hits']));
+                $queryEntry['Expires']=$this->oc['SourcePot\Datapool\Tools\MiscTools']->getDateTime('now','P10D',\SourcePot\Datapool\Root::DB_TIMEZONE);
+                $queryEntry=$this->oc['SourcePot\Datapool\Foundation\Database']->insertEntry($queryEntry);
+            }
         } else {
-            $serachResult='';
+            foreach($this->oc['SourcePot\Datapool\Foundation\Database']->entryIterator($queryEntry,FALSE,'Read','Date',TRUE) as $queryEntry){
+                $serachResult['Query']=$queryEntry['Content']['Query'];
+                break;
+            }
         }
+        $serachResult['Query']=$serachResult['Query']??'';
+        $serachResult['html']=$serachResult['html']??'';
         // compile html
         $arr['html']=(empty($arr['html']))?'':$arr['html'];
-        $arr['html'].=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'input','type'=>'text','value'=>($formData['val']['query']??''),'placeholder'=>'Enter your query here...','key'=>array('query'),'excontainer'=>TRUE,'callingClass'=>$arr['callingClass'],'callingFunction'=>$arr['callingFunction'],'style'=>array()));
+        $arr['html'].=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'input','type'=>'text','value'=>$serachResult['Query'],'placeholder'=>'Enter your query here...','key'=>array('Query'),'excontainer'=>TRUE,'callingClass'=>$arr['callingClass'],'callingFunction'=>$arr['callingFunction'],'style'=>array()));
         $arr['html'].=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'button','element-content'=>'Search','key'=>array('search'),'callingClass'=>$arr['callingClass'],'callingFunction'=>$arr['callingFunction'],'style'=>array()));
         $arr['html']=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'div','element-content'=>$arr['html'],'keep-element-content'=>TRUE,'style'=>array('float'=>'none','width'=>'max-content','margin'=>'5em auto')));
-        $arr['html'].=$serachResult;
+        $arr['html'].=$serachResult['html'];
         return $arr;
     }
 
-    private function getSerachResukltHtml(string $query):string{
+    private function getSerachResultHtml(string $query):array
+    {
         $entryCount=0;
         $selectors=array(array('Source'=>$this->oc['SourcePot\Datapool\GenericApps\Multimedia']->getEntryTable(),'Content'=>'%'.$query.'%'),
                          array('Source'=>$this->oc['SourcePot\Datapool\GenericApps\Forum']->getEntryTable(),'Content'=>'%'.$query.'%'),
                          array('Source'=>$this->oc['SourcePot\Datapool\GenericApps\Calendar']->getEntryTable(),'Content'=>'%'.$query.'%'),
                     );
-        $html='';
+        $arr['Query']=$query;
+        $arr['html']='';
+        $arr['Names']=array();
+        $arr['Hits']=array();
         foreach($selectors as $selector){
-            foreach($this->oc['SourcePot\Datapool\Foundation\Database']->entryIterator($selector,FALSE,'Read','Date',TRUE) as $entry){
-                $html.=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'div','element-content'=>'.','function'=>'loadEntry','source'=>$entry['Source'],'entry-id'=>$entry['EntryId'],'class'=>'home','style'=>array()));
+            foreach($this->oc['SourcePot\Datapool\Foundation\Database']->entryIterator($selector,FALSE,'Read','Date',TRUE,100,0) as $entry){
+                $arr['Names'][$entry['EntryId']]=$entry['Name'];
+                $arr['Hits'][$entry['EntryId']]=$entry['Source'];
+                $arr['html'].=$this->oc['SourcePot\Datapool\Foundation\Element']->element(array('tag'=>'div','element-content'=>'.','function'=>'loadEntry','source'=>$entry['Source'],'entry-id'=>$entry['EntryId'],'class'=>'home','style'=>array()));
             }
         }
-        return $html;
+        return $arr;
     }
 
     public function processSQLquery(array $stmtArr)
