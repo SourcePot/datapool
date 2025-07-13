002773b0e13d039a36ebf482f7afb3215f015fef
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2025-07-13 14:42:34 +0000

    Code style improved

--- src/php/Foundation/Element.php
+++ src/php/Foundation/Element.php
@@ -377,15 +377,16 @@ class Element{
 
     public function fileErrorCode2str($code):string
     {
-        $codeArr=array(0=>'There is no error, the file uploaded with success',
-                       1=>'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-                       2=>'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-                       3=>'The uploaded file was only partially uploaded',
-                       4=>'No file was uploaded',
-                       6=>'Missing a temporary folder',
-                       7=>'Failed to write file to disk.',
-                       8=>'A PHP extension stopped the file upload.',
-                       );
+        $codeArr=[
+            0=>'There is no error, the file uploaded with success',
+            1=>'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+            2=>'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+            3=>'The uploaded file was only partially uploaded',
+            4=>'No file was uploaded',
+            6=>'Missing a temporary folder',
+            7=>'Failed to write file to disk.',
+            8=>'A PHP extension stopped the file upload.',
+            ];
         $code=intval($code);
         if (isset($codeArr[$code])){return $codeArr[$code];} else {return '';}
     }
