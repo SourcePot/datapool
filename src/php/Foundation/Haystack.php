66ddd3ff4f2aa89a34f4b30b48049a6701d98968
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2026-04-11 18:35:56 +0000

    UI style improved

--- src/php/Foundation/Haystack.php
+++ src/php/Foundation/Haystack.php
@@ -72,7 +72,7 @@ class Haystack implements \SourcePot\Datapool\Interfaces\HomeApp{
         $html=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'input','type'=>'text','value'=>$serachResult['Query'],'placeholder'=>'Enter your query here...','key'=>['Query'],'excontainer'=>FALSE,'callingClass'=>$arr['callingClass'],'callingFunction'=>$arr['callingFunction'],'style'=>[]]);
         $html.=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'button','element-content'=>'Search','key'=>['search'],'callingClass'=>$arr['callingClass'],'callingFunction'=>$arr['callingFunction'],'style'=>[]]);
         $html=$this->searchBaseHtml($html);
-        $arr['html']=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$html,'keep-element-content'=>TRUE,'style'=>['float'=>'none','width'=>'min-content','display'=>'inline-block','border'=>'1px solid var(--blueH)','padding'=>'1rem','border-radius'=>'5px','background-color'=>'var(--bgColorA)']]);
+        $arr['html']=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$html,'keep-element-content'=>TRUE,'style'=>['float'=>'none','width'=>'min-content','display'=>'inline-block','border'=>'1px solid var(--blueH)','padding'=>'1rem 1rem 0 1rem','border-radius'=>'5px','background-color'=>'var(--bgColorA)']]);
         $arr['html']=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$arr['html'],'keep-element-content'=>TRUE,'style'=>['float'=>'none','width'=>'max-content','margin'=>'0 auto']]);
         $arr['html']=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$arr['html'],'keep-element-content'=>TRUE,'style'=>['float'=>'left','clear'=>'both','padding'=>'1rem 0','width'=>'inherit']]);
         // compile html - add result div
@@ -132,7 +132,7 @@ class Haystack implements \SourcePot\Datapool\Interfaces\HomeApp{
         $currentUserContent=$this->oc['SourcePot\Datapool\Root']->getCurrentUser()['Content']??[];
         foreach($this->oc['SourcePot\Datapool\Root']->getImplementedInterfaces('SourcePot\Datapool\Interfaces\Haystack') as $class){
             $classComps=explode('\\',$class); 
-            $arr=['value'=>!empty($currentUserContent['My search interests'][$class]),'element'=>['element-content'=>array_pop($classComps),'style'=>['font-size'=>'0.8rem','margin'=>'0.25rem 0']],'key'=>['searchIn',array_pop($classComps)],'callingClass'=>__CLASS__,'callingFunction'=>'searchBaseHtml','type'=>'checkbox','excontainer'=>FALSE];
+            $arr=['value'=>!empty($currentUserContent['My search interests'][$class]),'element'=>['element-content'=>array_pop($classComps),'style'=>['font-size'=>'0.8rem']],'key'=>['searchIn',array_pop($classComps)],'callingClass'=>__CLASS__,'callingFunction'=>'searchBaseHtml','type'=>'checkbox','excontainer'=>FALSE];
             $html.=$this->oc['SourcePot\Datapool\Tools\MiscTools']->bool2html($arr);
             if ($arr['value']){
                 $noneSelected=FALSE;
@@ -141,7 +141,7 @@ class Haystack implements \SourcePot\Datapool\Interfaces\HomeApp{
         if ($noneSelected){
             $html=$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'p','element-content'=>'No App selected: go to your "Admin → Account → 🔍" to configure Apps you want to search in future.','style'=>['color'=>'var(--attentionColor)'],'keep-element-content'=>FALSE]);
         } else {
-            $html=$preceedingHtml.$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$html,'keep-element-content'=>TRUE,'style'=>['float'=>'none','width'=>'max-content','margin'=>'0 auto']]);
+            $html=$preceedingHtml.$this->oc['SourcePot\Datapool\Foundation\Element']->element(['tag'=>'div','element-content'=>$html,'keep-element-content'=>TRUE,'style'=>['float'=>'none','display'=>'inline-block','width'=>'max-content','padding'=>'0.25rem 0 0 0']]);
         }
         return $html;
     }
