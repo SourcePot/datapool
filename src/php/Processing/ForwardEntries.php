b8b9eb07a709de40d4249c068eba33468d9d9817
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2026-09-08 19:25:38 +0000

    Bug fix

--- src/php/Processing/ForwardEntries.php
+++ src/php/Processing/ForwardEntries.php
@@ -285,7 +285,7 @@ class ForwardEntries implements \SourcePot\Datapool\Interfaces\Processor{
                 $result['Forwarded']['...'][$targetName]='...';
             }
         }
-        if ($wasForwarded && $removeForwardedEntries && !$testRun){
+        if ($wasForwarded && $removeForwardedEntries && empty($testRun)){
             $this->oc['SourcePot\Datapool\Foundation\Database']->deleteEntries($sourceEntry,TRUE);
         }
         if ($success){
