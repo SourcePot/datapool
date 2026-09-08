248a9360ad8e605ba8e53653972622b1774fa247
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2026-09-08 19:32:10 +0000

    Bug fix

--- src/php/Processing/ForwardEntries.php
+++ src/php/Processing/ForwardEntries.php
@@ -264,23 +264,24 @@ class ForwardEntries implements \SourcePot\Datapool\Interfaces\Processor{
         $moveForwardedEntry=boolval($params['Move entries']??0);
         $removeForwardedEntries=boolval($params['Remove forwarded entries']??0);
         foreach($forwardTo as $targetEntryId=>$conditionMet){
-            $skipRule=FALSE;
+            $skipTarget=FALSE;
             $targetName=array_search($targetEntryId,$base['targets']);
             $targetResultElement=$this->oc['SourcePot\Datapool\Tools\MiscTools']->bool2element($conditionMet,['style'=>['min-width'=>'unset','padding'=>'0']]);
             $targetResultElement=$this->oc['SourcePot\Datapool\Foundation\Element']->element($targetResultElement);
-            if ($conditionMet && !$skipRule){
+            if ($conditionMet && !$skipTarget){
                 $success=TRUE;
                 $wasForwarded=!$testRun;
                 $this->oc['SourcePot\Datapool\Foundation\Database']->moveEntryOverwriteTarget($sourceEntry,$base['entryTemplates'][$targetEntryId],TRUE,$testRun,!$moveForwardedEntry);
                 if ($moveForwardedEntry){
-                    $skipRule=TRUE;
+                    $skipTarget=TRUE;
                 }
             }
             $result['Forwarded']['<i>FORWARDED</i>'][$targetName]=(isset($result['Forwarded']['<i>FORWARDED</i>'][$targetName]))?($result['Forwarded']['<i>FORWARDED</i>'][$targetName]+intval($conditionMet)):intval($conditionMet);   
             if (count($result['Forwarded'])<self::MAX_RESULT_TABLE_ROW_COUNT){
+                if ($skipTarget){
+                    $equations[$targetEntryId]=$targetResultElement='∅';
+                }
                 $result['Forwarded'][$sourceEntry['Name']][$targetName]='<div style="">'.$equations[$targetEntryId].'<p style="clear:none;padding:0 0.3rem;">=</p>'.$targetResultElement.'</div>';
-            } else if ($skipRule){
-                $result['Forwarded'][$sourceEntry['Name']][$targetName]='∅';
             } else {
                 $result['Forwarded']['...'][$targetName]='...';
             }
